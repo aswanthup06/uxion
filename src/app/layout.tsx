f521cd0acd2613dbcf4cd87e65a92c3d6b3e8286
aswanthup06 <aswanthup06@gmail.com>
2025-08-28 17:32:50 +0530

    live2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+       <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P051RF09TL"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-P051RF09TL');
+            `,
+          }}
+        />
+      </head>
       
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
