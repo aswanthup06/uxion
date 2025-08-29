6377a826249b57592373eeb15468cb45b3e6d0f5
aswanthup06 <aswanthup06@gmail.com>
2025-08-29 19:52:57 +0530

    ads

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,6 +1,10 @@
 export default function Head() {
   return (
     <>
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-8853112000993716"
+      ></meta>
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8853112000993716"
